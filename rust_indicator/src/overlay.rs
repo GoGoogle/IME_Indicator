e7370482daa957cb4aca161b3ad67d2051bb25ed
GoGoogle <wowjoint@gmail.com>
2026-01-05 17:13:25 +0800

    Refactor overlay window handling and fix issues

--- rust_indicator/src/overlay.rs
+++ rust_indicator/src/overlay.rs
@@ -1,9 +1,10 @@
+//! GDI+ 悬浮窗渲染模块
 use std::ptr::{null_mut, null};
-use windows::core::PCWSTR;
+use windows::core::{w, PCWSTR};
 use windows::Win32::Foundation::{COLORREF, HWND, POINT, SIZE, LPARAM, LRESULT, WPARAM};
 use windows::Win32::Graphics::Gdi::{
     CreateCompatibleDC, CreateDIBSection, DeleteDC, DeleteObject, GetDC, ReleaseDC, SelectObject,
-    BITMAPINFO, BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS, UpdateLayeredWindow, ULW_ALPHA, BLENDFUNCTION, AC_SRC_OVER, AC_SRC_ALPHA
+    BITMAPINFO, BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
 };
 use windows::Win32::Graphics::GdiPlus::{
     GdipCreateFromHDC, GdipCreateSolidFill, GdipDeleteBrush, GdipDeleteGraphics,
@@ -12,10 +13,10 @@ use windows::Win32::Graphics::GdiPlus::{
 };
 use windows::Win32::UI::WindowsAndMessaging::{
     CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, PeekMessageW,
-    RegisterClassExW, SetWindowPos, ShowWindow, TranslateMessage,
+    RegisterClassExW, SetWindowPos, ShowWindow, TranslateMessage, UpdateLayeredWindow,
     HWND_TOPMOST, MSG, PM_REMOVE, SWP_NOACTIVATE, SWP_NOMOVE, SWP_NOSIZE, SW_HIDE, SW_SHOW,
-    WNDCLASSEXW, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TOPMOST,
-    WS_EX_TRANSPARENT, WS_POPUP,
+    ULW_ALPHA, WNDCLASSEXW, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TOPMOST,
+    WS_EX_TRANSPARENT, WS_POPUP, BLENDFUNCTION, AC_SRC_OVER, AC_SRC_ALPHA
 };
 
 pub struct IndicatorOverlay {
@@ -29,6 +30,11 @@ pub struct IndicatorOverlay {
     gdi_token: usize,
 }
 
+// 核心修复：手动定义一个符合 "system" 签名的 WNDPROC，防止编译器的类型推导错误
+extern "system" fn overlay_wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe { DefWindowProcW(hwnd, msg, wparam, lparam) }
+}
+
 impl IndicatorOverlay {
     pub fn new(_name: &str, size: i32, color_cn: u32, color_en: u32, color_caps: u32, offset_x: i32, offset_y: i32) -> Self {
         let mut token = 0;
@@ -37,20 +43,20 @@ impl IndicatorOverlay {
 
         let hwnd = unsafe {
             let h_inst = windows::Win32::System::LibraryLoader::GetModuleHandleW(None).unwrap();
-            let cls_name: Vec<u16> = "IndicatorOverlayClass\0".encode_utf16().collect();
+            let cls_name = w!("IndicatorOverlayClass");
             let wc = WNDCLASSEXW {
                 cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
-                lpfnWndProc: Some(DefWindowProcW),
+                lpfnWndProc: Some(overlay_wndproc), // 使用上面定义的 extern "system" 函数
                 hInstance: h_inst.into(),
-                lpszClassName: PCWSTR(cls_name.as_ptr()),
+                lpszClassName: cls_name,
                 ..Default::default()
             };
             RegisterClassExW(&wc);
             CreateWindowExW(
                 WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW,
-                PCWSTR(cls_name.as_ptr()), PCWSTR(null()), WS_POPUP,
+                cls_name, PCWSTR(null()), WS_POPUP,
                 0, 0, size, size, None, None, h_inst, None
-            ).expect("Failed to create window")
+            ).expect("Create Window Failed")
         };
 
         Self { hwnd, size, color_cn, color_en, color_caps, offset_x, offset_y, gdi_token: token }
@@ -61,7 +67,12 @@ impl IndicatorOverlay {
         unsafe {
             let screen_dc = GetDC(None);
             let mem_dc = CreateCompatibleDC(screen_dc);
-            let bmi = BITMAPINFO { bmiHeader: BITMAPINFOHEADER { biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32, biWidth: self.size, biHeight: self.size, biPlanes: 1, biBitCount: 32, biCompression: BI_RGB.0, ..Default::default() }, ..Default::default() };
+            let bmi = BITMAPINFO {
+                bmiHeader: BITMAPINFOHEADER {
+                    biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
+                    biWidth: self.size, biHeight: self.size, biPlanes: 1, biBitCount: 32, biCompression: BI_RGB.0, ..Default::default()
+                }, ..Default::default()
+            };
             let mut bits = null_mut();
             let h_bmp = CreateDIBSection(mem_dc, &bmi, DIB_RGB_COLORS, &mut bits, None, 0).unwrap();
             let old_bmp = SelectObject(mem_dc, h_bmp);
@@ -76,20 +87,42 @@ impl IndicatorOverlay {
             GdipDeleteGraphics(g);
 
             let dest = POINT { x: x + self.offset_x - self.size/2, y: y + caret_h + self.offset_y - self.size/2 };
-            let blend = BLENDFUNCTION { BlendOp: AC_SRC_OVER as u8, BlendFlags: 0, SourceConstantAlpha: 255, AlphaFormat: AC_SRC_ALPHA as u8 };
-            let _ = UpdateLayeredWindow(self.hwnd, screen_dc, Some(&dest), Some(&SIZE { cx: self.size, cy: self.size }), mem_dc, Some(&POINT::default()), COLORREF(0), Some(&blend), ULW_ALPHA);
+            let blend = BLENDFUNCTION { 
+                BlendOp: AC_SRC_OVER as u8, 
+                BlendFlags: 0, 
+                SourceConstantAlpha: 255, 
+                AlphaFormat: AC_SRC_ALPHA as u8 
+            };
+            
+            // 修复：UpdateLayeredWindow 在 0.58.0 中需要 &T 类型
+            let _ = UpdateLayeredWindow(
+                self.hwnd, 
+                screen_dc, 
+                Some(&dest), 
+                Some(&SIZE { cx: self.size, cy: self.size }), 
+                mem_dc, 
+                Some(&POINT::default()), 
+                COLORREF(0), 
+                Some(&blend), 
+                ULW_ALPHA
+            );
             
             SelectObject(mem_dc, old_bmp);
             let _ = DeleteObject(h_bmp);
             let _ = DeleteDC(mem_dc);
             ReleaseDC(None, screen_dc);
-            let _ = SetWindowPos(self.hwnd, HW_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+            
+            // 修复拼写：HW_TOPMOST -> HWND_TOPMOST
+            let _ = SetWindowPos(self.hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
             
             let mut msg = MSG::default();
-            while PeekMessageW(&mut msg, self.hwnd, 0, 0, PM_REMOVE).into() { TranslateMessage(&msg); DispatchMessageW(&msg); }
+            while PeekMessageW(&mut msg, self.hwnd, 0, 0, PM_REMOVE).into() {
+                let _ = TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            }
         }
     }
-    pub fn show(&self) { unsafe { ShowWindow(self.hwnd, SW_SHOW); } }
-    pub fn hide(&self) { unsafe { ShowWindow(self.hwnd, SW_HIDE); } }
+    pub fn show(&self) { unsafe { let _ = ShowWindow(self.hwnd, SW_SHOW); } }
+    pub fn hide(&self) { unsafe { let _ = ShowWindow(self.hwnd, SW_HIDE); } }
     pub fn cleanup(&self) { unsafe { let _ = DestroyWindow(self.hwnd); GdiplusShutdown(self.gdi_token); } }
 }
